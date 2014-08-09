011ccdb36f6beb1bdc3cd19f2968d7c100bd6702
Jeffrey Licht <jeff@podconsulting.com>
2014-08-09 16:23:05 -0400

    Detect links within HTML and annotate with (dummy) data attributes

--- mod_amber.c
+++ mod_amber.c
@@ -11,7 +11,7 @@ typedef struct {
     int   count;        /* Number of matching insertion positions and urls */
     int   *insert_pos;  /* Array - positions within the buffer where additional 
                            attributes should in inserted for matching hrefs */
-    char  *url;         /* Array - urls within the buffer. */
+    char  **url;        /* Array - urls within the buffer. */
 } amber_matches_t;
 
 /* Define prototypes of our functions in this module */
@@ -20,8 +20,8 @@ static int amber_handler(request_rec *r);
 static apr_status_t amber_filter(ap_filter_t *f, apr_bucket_brigade *bb);
 static int amber_should_apply_filter(ap_filter_t *f);
 static apr_bucket* amber_process_bucket(ap_filter_t *f, apr_bucket *bucket, const char *buffer, size_t buffer_size);
-static amber_matches_t find_links_in_bucket(ap_filter_t *f, apr_bucket *bucket);
-
+static amber_matches_t find_links_in_buffer(ap_filter_t *f, const char *buffer, size_t buffer_size);
+static size_t amber_insert_attributes(ap_filter_t *f, amber_matches_t links, const char *old_buffer, size_t old_buffer_size, const char *new_buffer, size_t new_buffer_size);
 /* Define our module as an entity and assign a function for registering hooks  */
 
 module AP_MODULE_DECLARE_DATA   amber_module =
@@ -166,7 +166,9 @@ static apr_status_t amber_filter(ap_filter_t *f, apr_bucket_brigade *bb)
  * @return true if the requests should be processed
  */
 static int amber_should_apply_filter(ap_filter_t *f) {
-    return (f && f->r && f->r->content_type && !strcmp("text/html", f->r->content_type));
+    
+    ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: File type: %s", f->r->content_type);
+    return (f && f->r && f->r->content_type && !strncmp("text/html", f->r->content_type, 9));
 }
 
 /** 
@@ -179,22 +181,40 @@ static int amber_should_apply_filter(ap_filter_t *f) {
  */
 static apr_bucket* amber_process_bucket(ap_filter_t *f, apr_bucket *bucket, const char *buffer, size_t buffer_size) {
 
-    ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Buffer contents: %s", buffer);
-    amber_matches_t links = find_links_in_bucket(f,bucket);
+    // ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Buffer contents %d %s", (int)buffer_size, buffer);
+    amber_matches_t links = find_links_in_buffer(f, buffer, buffer_size);
 
-    return bucket;
+    /* If there are no links to evaluate, just return the original bucket */
+    if (0 == links.count) {
+        return bucket;
+    }
+
+    /* If there are links to evaluate, create a new buffer for the updated links. 
+       Allocate additional memory for the HTML attributes we are going to add */
+    size_t AMBER_ATTRIBUTES_SIZE = 200;
+    size_t new_buffer_memory_allocated = buffer_size + (links.count * sizeof(char) * AMBER_ATTRIBUTES_SIZE);
+    char *new_buffer = apr_bucket_alloc( new_buffer_memory_allocated, f->c->bucket_alloc);
+
+    // *** UPDATE THE BUFFER HERE *** //
+    size_t new_bucket_size = amber_insert_attributes(f, links, buffer, buffer_size, new_buffer, new_buffer_memory_allocated);
+    
+    /* TODO - CHANGE THIS TO REFLECT THE ACTUAL SIZE OF THE NEW BUCKET */
+    apr_bucket *new_bucket = apr_bucket_heap_create(new_buffer, new_bucket_size, NULL, f->c->bucket_alloc);
+    return new_bucket;
 }
 
 /** 
- * Search the bucket for links that are candidates to be rewritten, using PCRE
+ * Search a buffer for links that are candidates to be rewritten, using PCRE
  * @param f the filter
- * @param bucket the bucket being processed
+ * @param buffer the contents of the bucket
+ * @param buffer_size the size of the buffer containing the bucket contents
  * @return amber_matches_t containing the number of links found, and arrays with the
  *         link URL and offset within the buffer where any rewriting should occur
  */
-static amber_matches_t find_links_in_bucket(ap_filter_t *f, apr_bucket *bucket) {
+static amber_matches_t find_links_in_buffer(ap_filter_t *f, const char *buffer, size_t buffer_size) {
 
-    amber_matches_t result = { .count = 0 };
+    amber_matches_t result = { .count = 0, .insert_pos = NULL, .url = NULL };
+    int MATCHES_CHUNK_SIZE = 5; /* Allocate memory for this many matches (will increase if required) */
     pcre *re;
     const char *pcre_error;
     int pcre_error_offset;
@@ -204,7 +224,7 @@ static amber_matches_t find_links_in_bucket(ap_filter_t *f, apr_bucket *bucket) 
 
     /* Compile the pattern to use */
     if (!(re = pcre_compile(pattern, PCRE_CASELESS, &pcre_error, &pcre_error_offset, NULL))) {
-        ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "PCRE compilation failed at offset %d: %s", pcre_error_offset, pcre_error);
+        ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: PCRE compilation failed at offset %d: %s", pcre_error_offset, pcre_error);
         return result;
     }
 
@@ -212,17 +232,121 @@ static amber_matches_t find_links_in_bucket(ap_filter_t *f, apr_bucket *bucket) 
     int subpattern_captures_count;
     int pcre_result;
     if ((pcre_result = pcre_fullinfo(re, NULL, PCRE_INFO_CAPTURECOUNT, &subpattern_captures_count)) < 0) {
-        ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "PCRE subpattern capture count failed with code %d", pcre_result);
+        ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: PCRE subpattern capture count failed with code %d", pcre_result);
         return result;
     }
 
+    /* Setup the structures into which pcre_exec will place the information about any matches */
+    int  pcre_results_vector_count = (subpattern_captures_count + 1) * 3;
+    int  *pcre_results_vector = apr_palloc(f->r->pool, pcre_results_vector_count * sizeof(int));
+
+    /* Walk through the buffer, until we stop getting matches or get to the end */
+    char   *pos = (char *)buffer;
+    size_t remaining_buffer = buffer_size;
     
+    do {
+        pcre_result = pcre_exec(re, NULL, pos, remaining_buffer, 0, 0, pcre_results_vector, pcre_results_vector_count); 
+        if (PCRE_ERROR_NOMATCH == pcre_result) { /* No matches */
+            break;
+        }
+        if (pcre_result < 0) {  /* An error occurred */
+            ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Error while matching regular expression %d", pcre_result);
+            break;
+        }
+        if (pcre_result > 0) { /* We have a match! */   
+            if (!result.insert_pos || !result.url) {
+                /* Lazy allocation of memory for our result structure when it's needed for the first time */
+                result.insert_pos = apr_palloc(f->r->pool, MATCHES_CHUNK_SIZE * sizeof(int));
+                result.url = apr_palloc(f->r->pool, MATCHES_CHUNK_SIZE * sizeof(char *));
+            } else if ((result.count % MATCHES_CHUNK_SIZE) == 0) {
+                /* Allocate another chunk of memory if needed */
+                int  *new_insert_pos = apr_palloc(f->r->pool, (result.count + MATCHES_CHUNK_SIZE) * sizeof(int));
+                char **new_url = apr_palloc(f->r->pool, (result.count + MATCHES_CHUNK_SIZE) * sizeof(char *));
+                memcpy(new_insert_pos, result.insert_pos, sizeof(int) * result.count);
+                memcpy(new_url, result.url, sizeof(char *) * result.count);
+                result.insert_pos = new_insert_pos;
+                result.url = new_url;
+            }   
 
+            /* pcre_results_vector[0] and pcre_results_vector[1] are for the capture group matching the full regex. 
+               pcre_results_vector[2] and pcre_results_vector[3] are for the first capture group within it 
+               The start of the full regesx is the insertion point, and the first capture group is the URL */
+            result.insert_pos[result.count] = (pos - buffer) + pcre_results_vector[0];
+            int url_size = (pcre_results_vector[3] - pcre_results_vector[2] + 1) * sizeof(char);
+            result.url[result.count] = apr_palloc(f->r->pool, url_size);
+            memcpy(result.url[result.count], pos + pcre_results_vector[2], url_size);
+            result.url[result.count][pcre_results_vector[3] - pcre_results_vector[2]] = 0; /* Null-terminate the string */
+            result.count++;
 
+            ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Match: %s", result.url[result.count-1]);
 
-
-// pcre *re;constchar *error;int erroffset;int ovector[OVECCOUNT];int rc, i;if (argc != 3)  {  printf("Two arguments required: a regex and a subject string\n");  return 1;  }/* Compile the regular expression in the first argument */re = pcre_compile(  argv[1],              /* the pattern */  0,                    /* default options */  &error,               /* for error message */  &erroffset,           /* for error offset */  NULL);                /* use default character tables */
+            pos += pcre_results_vector[1];
+            remaining_buffer = buffer - pos + buffer_size;
+        }
+    } while (pos < (buffer + buffer_size));
 
     return result;
 }
+
+/**
+ * Copy data from the old buffer to the new buffer, looking up link attributes and inserting them as we go
+ * Links which are not found in the database are enqueued for future caching
+ * @param f the filter
+ * @param links links detected in the old buffer
+ * @param old_buffer buffer with original content 
+ * @param old_buffer_size size of old_buffer
+ * @param new_buffer buffer which will contain the updated content (memory already allocated)
+ * @param new_buffer_size memory allocated for the new buffer 
+ * @return actual size of the new buffer
+ */
+static size_t amber_insert_attributes(ap_filter_t *f, amber_matches_t links, const char *old_buffer, size_t old_buffer_size, const char *new_buffer, size_t new_buffer_size) {
+
+    // Setup database for all the queries we're going to make
+
+    char *src = (char *)old_buffer;
+    char *dest = (char *)new_buffer;
+    int copy_size;
+
+    for (int i = 0; i < links.count; i++) {  
+
+        /* Copy the data up to the insertion point for the next match */
+        copy_size = links.insert_pos[i] + old_buffer - src;
+        if (copy_size + dest >= new_buffer + new_buffer_size) {
+            ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Not enough memory allocated for new buffer");
+            // TODO: Is there anything sensible we can do here?
+            break;
+        }        
+        memcpy(dest, src, copy_size);
+        src += copy_size;
+        dest += copy_size;
+
+        /* Get the attributes to insert, and copy them too */
+        char *insert = " data-testing='bananas' ";
+        copy_size = strlen(insert);
+        // ngx_int_t result = ngx_http_amber_get_attribute(r, sqlite_handle, sqlite_statement, links.url[i], insertion);
+        if (copy_size + dest >= new_buffer + new_buffer_size) {
+            ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Not enough memory allocated for new buffer");
+            // TODO: Is there anything sensible we can do here?
+            break;
+        }
+        memcpy(dest, insert, copy_size);
+        dest += copy_size;
+    }
+
+    /* Copy any remaining content after the last match */
+    if (src < old_buffer + old_buffer_size) {
+        copy_size = old_buffer + old_buffer_size - src;
+        if (dest + copy_size >= new_buffer + new_buffer_size) {
+            ap_log_error(APLOG_MARK, APLOG_EMERG, 0, f->r->server, "Amber: Not enough memory allocated for new buffer");
+        } else {
+            memcpy(dest, src, copy_size);
+            dest += copy_size;
+        }
+    }
+
+    /* Actual size of the new buffer */
+    return dest - new_buffer;
+}
+
+
 
